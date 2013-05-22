e622d0149e9133615cd0c65ec0f660631c68afcb
itsjoesullivan <itsjoesullivan@gmail.com>
2013-05-22 18:27:40 -0400

    bare test

--- test.js
+++ test.js
@@ -2,16 +2,9 @@ var Thread = require('thread');
 
 var routine = function() {
 
-	onmessage = function(e) {
-		postMessage(e.data);
-	};
 
-	thread.send('ready');
+
 
 };
 
 var thread = new Thread(routine);
-
-thread.on('ready', function() {
-	console.log('ready');
-});
