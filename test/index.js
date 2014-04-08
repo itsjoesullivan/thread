8d0d33b65fd5c4b780f81dabd2912f659a1f8329
itsjoesullivan <itsjoesullivan@gmail.com>
2014-04-08 16:53:47 -0400

    changed test

--- test/index.js
+++ test/index.js
@@ -9,8 +9,20 @@ var routine = function() {
 
 var thread = new Thread(routine);
 
+var returned = false;
+
 thread.on('pizza', function(pizza) {
-	console.log(pizza);
+  returned = pizza;
 });
 
+
 thread.send('order','cheese');
+
+setTimeout(function() {
+  if(!returned) {
+    throw "No returned!";
+  }
+  if (returned !== 'cheese pizza') {
+    throw "Didn't return the correct thing!";
+  }
+}, 100);
